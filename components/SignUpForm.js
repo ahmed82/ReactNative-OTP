96eb13c53a174c9cb7c804e0bcb1ee9d9092d4c7
Ahmed AL Salih <a.alsalih2@gmail.com>
2019-05-18 18:58:35 -0400

    create componant for SignUpForm

---
+++ components/SignUpForm.js
@@ -0,0 +1,21 @@
+import React, { Components } from 'react';
+import { View, Text } from 'react-native';
+import { FormLabel, FormInput, Button } from 'react-native-elements';
+
+class SingUpForm extends Components { 
+    constructor (props) {
+        super(props);
+        this.state = { phone : ''}
+    }
+    render() {
+        return (
+            <View>
+                <FormLabel>Enter Phone Number</FormLabel>
+                <FormInput />
+                <Button title="Submit" />
+            </View>
+        )
+    }
+}
+
+export default SingUpForm;
