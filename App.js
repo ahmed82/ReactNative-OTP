cab5ad378850b20f44b3620f9669159e0b859950
Ahmed Al Salih <ahmed82@users.noreply.github.com>
2019-05-27 01:24:42 -0400

    Update App.js

--- App.js
+++ App.js
@@ -7,13 +7,13 @@ import firebase from 'firebase'
 export default class App extends React.Component {
     componentDidMount() {
       const firebaseConfig = {
-        apiKey: "AIzaSyB3F-QBw0kbYU6W5KpzXZas6g5aHuiOo2o",
+        apiKey: "xxx",
         authDomain: "one-time-password-f996d.firebaseapp.com",
         databaseURL: "https://one-time-password-f996d.firebaseio.com",
         projectId: "one-time-password-f996d",
         storageBucket: "one-time-password-f996d.appspot.com",
         messagingSenderId: "758774069472",
-        appId: "1:758774069472:web:d131ecc3de7d132e"
+        appId: "1:xxx:web:xxxx"
       };
       // Initialize Firebase
       firebase.initializeApp(firebaseConfig);
